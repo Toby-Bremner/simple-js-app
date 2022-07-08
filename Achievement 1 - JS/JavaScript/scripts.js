ec889f30483003332acb4d418a853b554a86caf3
Toby-Bremner <bremner.tobias@gmail.com>
2022-07-08 09:12:37 -0300

    Update for Exercise 1.3
    Published array to index
    Added 2 loops to the array
    Updated CSS with minor changes
    Added my logo

--- Achievement 1 - JS/JavaScript/scripts.js
+++ Achievement 1 - JS/JavaScript/scripts.js
@@ -1,7 +1,34 @@
+//Height is in meters. Weight is in kilograms. 
+
 let pokemonList = [
     {
         name: 'Bulbasaur',
         attack: 49,
+        defence: 49,
+        height: 0.7,
+        weight: 6.9,
+        types: [
+            'grass', 'poison'
+        ]
+    },
+
+    {
+        name: 'Ivysaur',
+        attack: 62,
+        defence: 63,
+        height: 1,
+        weight: 13,
+        types: [
+            'grass', 'poison'
+        ]
+    },
+
+    {
+        name: 'Venusaur',
+        attack: 82,
+        defence: 83,
+        height: 2,
+        weight: 100,
         types: [
             'grass', 'poison'
         ]
@@ -10,24 +37,116 @@ let pokemonList = [
     {
         name: 'Charmander',
         attack: 52,
+        defence: 43,
+        height: 0.6,
+        weight: 8.5,
         types: [
             'fire'
         ]
     },
 
     {
-        name: 'squirtle',
+        name: 'Charmeleon',
+        attack: 64,
+        defence: 58,
+        height: 1.1,
+        weight: 19,
+        types: [
+            'fire'
+        ]
+    },
+
+    {
+        name: 'Charizard',
+        attack: 84,
+        defence: 78,
+        height: 1.7,
+        weight: 90.5,
+        types: [
+            'fire', 'flying'
+        ]
+    },
+
+ 
+    {
+        name: 'Squirtle',
         attack: 48,
+        defence: 65,
+        height: 0.5,
+        weight: 9,
         types: [
             'water'
         ]
     },
 
     {
-        name: 'butterfree',
+        name: 'Wartortle',
+        attack: 63,
+        defence: 80,
+        height: 1,
+        weight: 22.5,
+        types: [
+            'water'
+        ]
+    },
+
+    {
+        name: 'Blastoise',
+        attack: 83,
+        defence: 100,
+        height: 1.6,
+        weight: 85.5,
+        types: [
+            'water'
+        ]
+    },
+
+    {
+        name: 'Caterpie',
+        attack: 30,
+        defence: 35,
+        height: 1,
+        weight: 0.3,
+        types: [
+            'bug'
+        ]
+    },
+
+    {
+        name: 'Metapod',
+        attack: 20,
+        defence: 55,
+        height: 0.7,
+        weight: 9.9,
+        types: [
+            'bug'
+        ]
+    },
+
+    {
+        name: 'Butterfree',
         attack: 45,
+        defence: 50,
+        height: 1.1,
+        weight: 32,
         types: [
             'Bug', 'flying'
         ]
-    }
+    },
 ]
+
+for (let i=0; i<pokemonList.length; i++)
+{
+    document.write(pokemonList[i].name + " height is "+ pokemonList[i].height +" ");
+
+
+    if(pokemonList[i].height >=2){
+        document.write(` and Venusaur is the biggest`);
+    }
+    document.write('<br>');
+
+    if(pokemonList[i].height <=0.5){
+        document.write(` and Squirtle is the shortest <br>`);
+    }
+    document.write('<br>');
+}
